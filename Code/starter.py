467b1bc2756b133dd65f6d26cff9005e6a6ec824
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-22 15:22:34 +0200

    Update starter.py

--- Code/starter.py
+++ Code/starter.py
@@ -1,19 +1,20 @@
 from TrainingAllModel import train
 
+"""
+main script
+
+"""
+
 DATA_DIR = '../../Files/All/'  #### Dataset folder
 MODEL_SAVE_DIR = '../../TrainedModels'  #### Models folder
-INFERENCE = False
-STEPS = 1
-LR = 3e-4
+INFERENCE = False ### if no training needed
+STEPS = 1 ### number of timesteps per iteration
+LR = 3e-4 ### initial learning rate
 batch_size = 512
 
-# keys = ['C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4']
-
 keys = ['A0', 'B1', 'C2', 'D3', 'E4', 'F5', 'G6', 'A#7']
-keys = ['A0']  # 'A0',
 
-models = ['LSTM', 'CNN', 'S6']  # 'SSM'
-models = ['S6']  # 'SSM'
+models = ['LSTM', 'S6']
 
 for key in keys:
     for model in models:
